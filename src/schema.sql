d998ac1a4c2193f813c73e3f1cd80d2f656162aa
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-01-28 15:36:16 +0000

    Done the schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,2 +1,148 @@
 .open fittrackpro.db
 .mode column
+
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+    location_id VARCHAR PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    address VARCHAR,
+    phone_number VARCHAR CHECK (length(replace(phone_number,' ',''))>= 10),
+    email VARCHAR CHECK (email LIKE '%_@_%._%'),
+    opening_hours VARCHAR CHECK (opening_hours LIKE '__:__-__:__')
+
+);
+DROP TABLE IF EXISTS members;
+CREATE TABLE members (
+    member_id VARCHAR PRIMARY KEY NOT NULL,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR CHECK (email LIKE '%_@_%._%'),
+    phone_number VARCHAR CHECK (length(replace(phone_number,' ',''))>= 10),
+    date_of_birth DATE,
+    join_date DATE,
+    emergency_contact_name VARCHAR,
+    emergency_contact_phone VARCHAR CHECK (length(replace(emergency_contact_phone,' ',''))>= 10)
+);
+
+DROP TABLE IF EXISTS STAFF;
+CREATE TABLE staff(
+    staff_id VARCHAR PRIMARY KEY NOT NULL,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR CHECK (email LIKE '%_@_%._%'), 
+    phone_number VARCHAR CHECK (length(replace(phone_number,' ',''))>= 10),
+    position VARCHAR CHECK (position IN('Trainer','Manager','Receptionist','Maintenance')),
+    hire_date DATE, 
+    location_id VARCHAR, 
+    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+);
+
+DROP TABLE IF EXISTS equipment;
+CREATE TABLE equipment(
+    equipment_id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    type VARCHAR CHECK (type IN('Cardio','Strength')),
+    purchase_date DATE, 
+    last_maintenance_date DATE, 
+    next_maintenance_date DATE,
+    location_id VARCHAR,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes(
+    class_id INTEGER PRIMARY KEY NOT NULL, 
+    name VARCHAR,
+    description VARCHAR,
+    capacity INTEGER,
+    duration INTEGER,  
+    location_id VARCHAR,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+);
+
+DROP TABLE IF EXISTS class_schedule;
+CREATE TABLE class_schedule(
+    schedule_id INTEGER PRIMARY KEY NOT NULL,
+    class_id VARCHAR, 
+    staff_id VARCHAR,
+    start_time TIME, 
+    end_time TIME,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+
+DROP TABLE IF EXISTS memberships;
+CREATE TABLE memberships(
+    membership_id INTEGER PRIMARY KEY NOT NULL,
+    member_id,
+    type VARCHAR,
+    start_date DATE, 
+    end_date DATE,
+    status CHECK (status IN ('Active','Inactive')),
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+);
+
+DROP TABLE IF EXISTS attendance;
+CREATE TABLE attendance(
+    attendance_id INTEGER PRIMARY KEY NOT NULL,
+    member_id,
+    location_id,
+    check_in_time TIME, 
+    check_out_time TIME,
+    FOREIGN KEY(location_id) REFERENCES location(location_id),
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+);
+
+DROP TABLE IF EXISTS class_attendance;
+CREATE TABLE class_attendance(
+    class_attendance_id INTEGER PRIMARY KEY NOT NULL,
+    schedule_id VARCHAR,
+    member_id VARCHAR,
+    FOREIGN KEY(member_id) REFERENCES member(member_id),    
+    FOREIGN KEY(schedule_id) REFERENCES schedule(schedule_id)
+);
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments(
+    payment_id VARCHAR PRIMARY KEY NOT NULL,
+    member_id VARCHAR,
+    amount FLOAT,
+    payment_date TIME,
+    payment_method CHECK (payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal','Cash')),
+    payment_type CHECK (payment_type IN ('Monthly membership fee', 'Day pass')),
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+);
+
+DROP TABLE IF EXISTS personal_training_sessions;
+CREATE TABLE personal_training_sessions(
+    session_id VARCHAR PRIMARY KEY NOT NULL,
+    member_id VARCHAR,
+    staff_id VARCHAR,
+    session_date DATE,
+    start_time TIME,
+    end_time TIME,
+    notes VARCHAR,
+    FOREIGN KEY(staff_id) REFERENCES staff(staff_id),    
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+);
+
+DROP TABLE IF EXISTS member_health_metrics;
+CREATE TABLE member_health_metrics(
+    metric_id VARCHAR PRIMARY KEY NOT NULL,
+    member_id VARCHAR,
+    measurement_date DATE, 
+    weight REAL,
+    body_fat_percentage REAL,
+    muscle_mass REAL,
+    bmi REAL,
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+);
+
+DROP TABLE IF EXISTS equipment_maintence_log;
+CREATE TABLE equipment_maintence_log(
+    log_id VARCHAR PRIMARY KEY NOT NULL,
+    equipment_id VARCHAR,
+    maintenance_date DATE,
+    description VARCHAR,
+    FOREIGN KEY(equipment_id) REFERENCES equipment(equipment_id)
+);
