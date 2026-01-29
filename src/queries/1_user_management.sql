dcbf92b57b9057533c57bd6ac6b4c61e562aa8f3
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-01-29 14:46:19 +0000

    Done up to 1.3

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,12 +2,19 @@
 .mode column
 
 -- 1.1
+SELECT member_id,first_name,last_name,email,join_date
+FROM members;
 
 
 -- 1.2
+UPDATE members
+SET phone_number='07000 100005',email='emily.jones.updated@email.com'
+WHERE member_id=5;
 
 
 -- 1.3
+SELECT COUNT(*)
+FROM members
 
 
 -- 1.4
