19867647a1f9a9c75d6bf4b2962be43022f5a08e
Mahdi Salam <mahdi.salam@ada.ac.uk>
2026-02-01 15:32:46 +0000

    Done up to task 4.1

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -2,10 +2,19 @@
 .mode column
 
 -- 3.1 
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN '2025-01-01' AND date('2025-01-01','+30 days');
 
 
 -- 3.2 
-
+SELECT type as equipment_type, count(type) as count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
+SELECT type AS equipment_type, avg(julianday('now') - julianday(purchase_date)) AS avg_age_days -- Is this till current day time or till next maintenance date?
+FROM equipment
+GROUP BY type;
+
 
--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -2,7 +2,12 @@
 .mode column
 
 -- 4.1 
-
+SELECT class_schedule.class_id,name AS class_name, first_name||' '||last_name AS instructor_name
+FROM class_schedule
+JOIN classes
+ON class_schedule.class_id=classes.class_id 
+JOIN staff
+ON class_schedule.staff_id=staff.staff_id;
 
 -- 4.2 
 
