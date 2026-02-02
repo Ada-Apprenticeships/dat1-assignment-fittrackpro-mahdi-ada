2a12885514df7479c5b077fc6c53291b657e3b52
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-02-02 17:53:03 +0000

    Done up tp 4.2

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -10,8 +10,14 @@ JOIN staff
 ON class_schedule.staff_id=staff.staff_id;
 
 -- 4.2 
-
-
+SELECT classes.class_id,classes.name,class_schedule.start_time, class_schedule.end_time, (classes.capacity-COUNT(class_attendance.member_id) ) AS available_spots 
+FROM class_schedule
+JOIN classes
+ON class_schedule.class_id=classes.class_id
+LEFT JOIN class_attendance --This ensures even if we don't have any attendance the query will still work and not forget about this class
+ON class_schedule.schedule_id=class_attendance.schedule_id
+WHERE start_time LIKE '2025-02-01%' 
+GROUP BY class_schedule.schedule_id
 -- 4.3 
 
 
