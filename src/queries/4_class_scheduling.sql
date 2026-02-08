30a6132908750e9ad279b63d85db96bcd5f26f03
Mahdi Salam <mahdi.salam@ada.ac.uk>
2026-02-08 14:07:54 +0000

    Done tasl 4

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -17,16 +17,29 @@ ON class_schedule.class_id=classes.class_id
 LEFT JOIN class_attendance --This ensures even if we don't have any attendance the query will still work and not forget about this class
 ON class_schedule.schedule_id=class_attendance.schedule_id
 WHERE start_time LIKE '2025-02-01%' 
-GROUP BY class_schedule.schedule_id
+GROUP BY class_schedule.schedule_id;
 -- 4.3 
 INSERT INTO class_attendance(class_attendance_id,schedule_id,member_id,attendance_status)
-VALUES ()
+VALUES (16,1,11,'Registered');
 
 -- 4.4 
-
+DELETE FROM class_attendance
+WHERE member_id=3 AND schedule_id=7;
 
 -- 4.5 
+SELECT classes.class_id,classes.name AS class_name,COUNT(class_attendance.attendance_status) AS registration_count
+FROM class_attendance
+JOIN class_schedule 
+ON class_attendance.schedule_id=class_schedule.schedule_id
+JOIN classes
+ON class_schedule.class_id=classes.class_id
+WHERE class_attendance.attendance_status = 'Registered'
+GROUP BY classes.class_id
+ORDER BY registration_count DESC 
+LIMIT 1;
+
 
 
 -- 4.6 
+SELECT CAST((SELECT COUNT(*) FROM class_attendance)AS FLOAT) / CAST((SELECT COUNT(*) FROM members) AS FLOAT) AS avg_classes;
 
