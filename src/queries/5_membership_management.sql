50f0777278710f1b93fa914baefbc91d6e21eaa0
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-02-11 17:53:37 +0000

    5.1

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -2,7 +2,11 @@
 .mode column
 
 -- 5.1 
-
+SELECT memberships.member_id,members.first_name,members.last_name,memberships.type AS membership_type,memberships.start_date AS join_date
+FROM memberships
+JOIN members 
+ON memberships.member_id=members.member_id
+WHERE memberships.status= 'Active';
 
 -- 5.2 
 
