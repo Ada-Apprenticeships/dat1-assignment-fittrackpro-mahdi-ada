1990f6401e74d800d5b211b883f771b96dc5bac6
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-02-13 16:27:15 +0000

    Done up to 6.1

--- src/queries/6_attendance_tracking.sql
+++ src/queries/6_attendance_tracking.sql
@@ -2,10 +2,15 @@
 .mode column
 
 -- 6.1 
-
-
+INSERT INTO attendance(attendance_id,member_id,location_id,check_in_time)
+VALUES (4,7,1,'2025-02-14 16:30:00');
+--check if i was meant to get NULL for check_out_time
 -- 6.2 
-
+SELECT date(check_in_time) AS visit_date,attendance.check_in_time,attendance.check_out_time
+FROM attendance
+JOIN members
+ON attendance.member_id=members.member_id
+WHERE attendance.member_id=5;
 
 -- 6.3 
 
