637598a24a02048ece6a8ac058442919def8a678
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-02-22 14:48:39 +0000

    Done up to 6.3 need to finish off

--- src/queries/6_attendance_tracking.sql
+++ src/queries/6_attendance_tracking.sql
@@ -6,15 +6,31 @@ INSERT INTO attendance(attendance_id,member_id,location_id,check_in_time)
 VALUES (4,7,1,'2025-02-14 16:30:00');
 --check if i was meant to get NULL for check_out_time
 -- 6.2 
-SELECT date(check_in_time) AS visit_date,attendance.check_in_time,attendance.check_out_time
+SELECT DATE(check_in_time) AS visit_date,attendance.check_in_time,attendance.check_out_time
 FROM attendance
-JOIN members
-ON attendance.member_id=members.member_id
-WHERE attendance.member_id=5;
---complete this 
+WHERE attendance.member_id=5
+ORDER BY visit_date;
 
 -- 6.3 
-
-
+SELECT 
+    CASE STRFTIME('%w',check_in_time)
+    WHEN '0' THEN 'Sunday'
+    WHEN '1' THEN 'Monday'
+    WHEN '2' THEN 'Tuesday'
+    WHEN '3' THEN 'Wednesday'
+    WHEN '4' THEN 'Thursday'
+    WHEN '5' THEN 'Friday'
+    WHEN '6' THEN 'Saturday'
+    END AS day_of_week,
+    COUNT(*) AS visit_count 
+FROM attendance
+GROUP BY day_of_week
+ORDER BY visit_count DESC
+LIMIT 1;
 -- 6.4 
+SELECT locations.name as location_name, COUNT(attendance.location_id) as avg_daily_attendance
+FROM locations 
+LEFT JOIN attendance
+ON locations.location_id=attendance.location_id
+GROUP BY locations 
 
