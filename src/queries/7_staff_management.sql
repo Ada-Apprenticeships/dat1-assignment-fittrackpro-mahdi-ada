b38af26f64744a1842dbe25d2ef4366ac31cb2ad
mahdi-ada <mahdi.salam@ada.ac.uk>
2026-02-26 15:44:31 +0000

    Finished task 7

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -3,15 +3,15 @@
 
 -- 7.1 
 SELECT staff_id, first_name,last_name,position AS role
-FROM staff 
+FROM staff; 
 
 
 -- 7.2 
-SELECT staff.staff_id as trainer_id, staff.first_name ||' '|| staff.last_name AS trainer_name, COUNT(*) AS session_count 
+SELECT staff.staff_id AS trainer_id, staff.first_name ||' '|| staff.last_name AS trainer_name, COUNT(*) AS session_count 
 FROM staff
-JOIN personal_training_session
-ON staff.staff_id=personal_training_session.staff_id
-WHERE session_date BETWEEN '2025-01-20' AND DATE('2025-01-20','+30 days') AND session_count>=1
+JOIN personal_training_sessions
+ON staff.staff_id=personal_training_sessions.staff_id
+WHERE session_date BETWEEN '2025-01-20' AND DATE('2025-01-20','+30 days')  
 GROUP BY trainer_id
+HAVING session_count>=1;
 
---finish this off
